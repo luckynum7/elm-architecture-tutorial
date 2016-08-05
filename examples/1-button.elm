fdeb7140949a5c9f63c10d0519c652ab6afb9e31
Mark Huo <huomian@outlook.com>
2016-08-05 18:23:52 +0800

    add Reset

--- examples/1-button.elm
+++ examples/1-button.elm
@@ -1,27 +1,29 @@
+module Main exposing (..)
+
 import Html exposing (Html, button, div, text)
 import Html.App as Html
 import Html.Events exposing (onClick)
 
 
-
 main =
-  Html.beginnerProgram
-    { model = model
-    , view = view
-    , update = update
-    }
+    Html.beginnerProgram
+        { model = model
+        , view = view
+        , update = update
+        }
 
 
 
 -- MODEL
 
 
-type alias Model = Int
+type alias Model =
+    Int
 
 
 model : Model
 model =
-  0
+    0
 
 
 
@@ -29,18 +31,22 @@ model =
 
 
 type Msg
-  = Increment
-  | Decrement
+    = Increment
+    | Decrement
+    | Reset
 
 
 update : Msg -> Model -> Model
 update msg model =
-  case msg of
-    Increment ->
-      model + 1
+    case msg of
+        Increment ->
+            model + 3
 
-    Decrement ->
-      model - 1
+        Decrement ->
+            model - 4
+
+        Reset ->
+            model - model
 
 
 
@@ -49,8 +55,9 @@ update msg model =
 
 view : Model -> Html Msg
 view model =
-  div []
-    [ button [ onClick Decrement ] [ text "-" ]
-    , div [] [ text (toString model) ]
-    , button [ onClick Increment ] [ text "+" ]
-    ]
+    div []
+        [ button [ onClick Decrement ] [ text "- 4" ]
+        , div [] [ text (toString model) ]
+        , button [ onClick Increment ] [ text "+ 3" ]
+        , button [ onClick Reset ] [ text "reset" ]
+        ]
