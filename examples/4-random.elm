4995f3cda8acfd7ff4e77f347a2482bebb034da2
Mark Huo <huomian@outlook.com>
2016-08-10 19:03:37 +0800

    Add a second die and have them both roll at the same time.

--- examples/4-random.elm
+++ examples/4-random.elm
@@ -4,10 +4,7 @@ import Html exposing (..)
 import Html.App as Html
 import Html.Attributes exposing (..)
 import Html.Events exposing (..)
-import Random
-
-
--- import Style exposing (..)
+import Random exposing (..)
 
 
 main =
@@ -24,13 +21,14 @@ main =
 
 
 type alias Model =
-    { dieFace : Int
+    { diceFace : Int
+    , otherFace : Int
     }
 
 
 init : ( Model, Cmd Msg )
 init =
-    ( Model 1, Cmd.none )
+    ( Model 1 1, Cmd.none )
 
 
 
@@ -39,17 +37,17 @@ init =
 
 type Msg
     = Roll
-    | NewFace Int
+    | NewFace ( Int, Int )
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
         Roll ->
-            ( model, Random.generate NewFace (Random.int 1 6) )
+            ( model, generate NewFace (pair (int 1 6) (int 1 6)) )
 
-        NewFace newFace ->
-            ( Model newFace, Cmd.none )
+        NewFace ( newFace, otherFace ) ->
+            ( Model newFace otherFace, Cmd.none )
 
 
 
@@ -68,18 +66,20 @@ subscriptions model =
 view : Model -> Html Msg
 view model =
     div []
-        [ div [ toStyle model ] []
-        , h1 [] [ text (toString model.dieFace) ]
+        [ h1 [] [ text (toString model.diceFace) ]
+        , div [ toStyle model.diceFace ] []
+        , h1 [] [ text (toString model.otherFace) ]
+        , div [ toStyle model.otherFace ] []
         , button [ onClick Roll ] [ text "Roll" ]
         ]
 
 
-toStyle : Model -> Attribute msg
-toStyle model =
+toStyle : Int -> Attribute msg
+toStyle face =
     -- 2,-213, -428, -643, -858, -1073 | 217 - 215 * n(1-6)
     let
         x =
-            toString (217 - 215 * model.dieFace)
+            toString (217 - 215 * face)
     in
         style
             [ ( "background", "url(\"/dice.png\") " ++ x ++ "px -215px no-repeat" )
